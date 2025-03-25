fdb6ddd48fcc896054389b657d90046d1bdf6fb6
Vinicius Cainelli <vicainelli+github@gmail.com>
2025-03-25 07:13:46 +0100

    feat: add tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // integrations: [tailwind()],
+
+  vite: {
+    css: {
+      transformer: "lightningcss",
+    },
+    plugins: [tailwindcss()]
+  }
 });
