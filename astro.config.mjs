1498ab5fdaf1ef8b8005106f352564db374ceeef
Vinicius Cainelli <vicainelli+github@gmail.com>
2025-03-25 07:17:59 +0100

    refactor: update astro and tailwind (#160)
    * feat: update astro and tailwindcss
    
    * feat: add tailwind v4
    
    * chore: remove tailwind plugin
    
    * chore: ignore .astro
    
    * chore: ignore .astro files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  vite: {
+    css: {
+      transformer: "lightningcss",
+    },
+    plugins: [tailwindcss()]
+  }
 });
